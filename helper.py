bb28c20c635730f22cc328dac5f5bfe3af79f585
Denis Lebidka <hurdos@gmail.com>
2025-07-15 16:42:04 +0300

    Added sql create table and delete bond by figi.

--- helper.py
+++ helper.py
@@ -14,6 +14,21 @@ class SqlAdapter():
     def __init__(self):
         self.conn = sqlite3.connect('invest.db3')
         self.cursor = self.conn.cursor()
+        # create table bonds
+        self.cursor.execute('''
+        CREATE TABLE IF NOT EXISTS "bonds" (
+            "figi"	TEXT NOT NULL UNIQUE,
+            "name"	TEXT,
+            "quantity"	INTEGER NOT NULL,
+            "nominal"	INTEGER NOT NULL,
+            "cost"	NUMERIC NOT NULL,
+            "coupon_payout"	NUMERIC NOT NULL,
+            "avg_price"	INTEGER NOT NULL,
+            "current_price"	INTEGER NOT NULL,
+            PRIMARY KEY("figi")
+        )
+        ''')
+        self.conn.commit()
 
     def is_bond_exists(self, figi=''):
         self.cursor.execute('SELECT figi FROM bonds WHERE figi = ?', (figi,))
@@ -34,6 +49,12 @@ class SqlAdapter():
         values = values + (figi,)
         # print(values)
         self.cursor.execute('UPDATE bonds SET quantity = ?, cost = ?, coupon_payout = ?, avg_price = ?, current_price = ? WHERE figi = ?', values)
+        self.conn.commit()
+
+    def delete_bond(self, figi=''):
+        # print(figi)
+        self.cursor.execute('DELETE FROM bonds WHERE figi = ?', (figi,))
+        # print(self.cursor.rowcount)
         self.conn.commit()
 
     def close(self):
